fdebda38ce25a1b3a3070bf0623d722b3ceeb98e
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 16:50:14 -0300

    feat(back/auth/infra): add bcrypt hash implementation

---
+++ backend/src/modules/auth/infra/hash/bcrypthash.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+import { Hash } from '../../application/interfaces/hash';
+
+export class BcryptHash implements Hash {
+  async make(value: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(value, salt);
+  }
+
+  async compare(value: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(value, hashed);
+  }
+}
