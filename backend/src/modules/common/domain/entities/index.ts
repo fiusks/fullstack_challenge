8b66ca7441e4fe125a700daad6762970cfbee3d2
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 11:43:13 -0300

    chore(back/common): create an error folder

--- backend/src/modules/common/domain/entities/index.ts
+++ backend/src/modules/common/domain/entities/index.ts
@@ -1,5 +1,1 @@
 export * from './base-entity';
-export * from './base-error';
-export * from './client-side.error';
-export * from './not-found.error';
-export * from './unprocessable-entity.error';
---
+++ backend/src/modules/common/domain/errors/index.ts
@@ -0,0 +1,4 @@
+export * from './base-error';
+export * from './client-side.error';
+export * from './not-found.error';
+export * from './unprocessable-entity.error';
