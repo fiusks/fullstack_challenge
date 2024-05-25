b5dcddef1fb20da742641f8e16daf18f904dd682
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 20:20:00 -0300

    refactor(back/orders/domain): remove update from OrdeRepository

--- backend/src/modules/orders/domain/repositories/order.repository.ts
+++ backend/src/modules/orders/domain/repositories/order.repository.ts
@@ -2,7 +2,6 @@ import { Order } from '../entities/order.entity';
 
 export interface OrderRepository {
   create(input: Order.CreateProps): Promise<Order>;
-  update(input: Order.CreateProps): Promise<Order>;
   delete(id: string): Promise<void>;
   findById(id: string): Promise<Order | null>;
   findByCustomerId(id: string): Promise<Order | null>;
