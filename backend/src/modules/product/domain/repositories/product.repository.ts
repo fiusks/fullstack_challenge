ee874114e2e4637e02a8712766aaaf87d7ff94e6
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 11:48:49 -0300

    feat(back/product/domain): add productRepository

---
+++ backend/src/modules/product/domain/repositories/product.repository.ts
@@ -0,0 +1,10 @@
+import { Product } from '../entities';
+
+export interface ProductRepository {
+  create(product: Product): Promise<Product>;
+  findById(id: string): Promise<Product | null>;
+  findByName(name: string): Promise<Product | null>;
+  findManyByCategoryId(categoryId: string): Promise<Product[]>;
+  findAll(): Promise<Product[]>;
+  update(product: Product): Promise<Product>;
+}
