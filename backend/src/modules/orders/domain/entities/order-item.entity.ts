6765415221cf11e0b1a610b852fc520a4419adc9
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 20:37:49 -0300

    feat(back/orders/domain): add orders entities

---
+++ backend/src/modules/orders/domain/entities/order-item.entity.ts
@@ -0,0 +1,67 @@
+import { OrderItemAmount } from '../value-objects/order-item-amount.vo';
+import { BaseEntity } from 'src/modules/common/domain';
+import { Product } from 'src/modules/product';
+
+export class OrderItem extends BaseEntity {
+  public static get validator() {
+    return BaseEntity.baseValidator.extend({
+      product: Product.validator.transform((props) => new Product(props)),
+      amount: OrderItemAmount.validator.transform(
+        (amount) => new OrderItemAmount(amount),
+      ),
+    });
+  }
+
+  public static create(props: OrderItem.CreateProps): OrderItem {
+    return new OrderItem(OrderItem.validator.parse(props));
+  }
+
+  public toJSON(): OrderItem.JSON {
+    return {
+      id: this.id.toJSON(),
+      product: this.product.toJSON(),
+      amount: this.amount.toJSON(),
+      createdAt: this.createdAt.toJSON(),
+      updatedAt: this.updatedAt.toJSON(),
+    };
+  }
+
+  public get price(): number {
+    // TODO use BigDecimal
+    return this.product.price * this.amount.value;
+  }
+
+  public get product(): Product {
+    return this.#product;
+  }
+
+  public get amount(): OrderItemAmount {
+    return this.#amount;
+  }
+
+  constructor(props: OrderItem.Props) {
+    super(props.id, props.createdAt, props.updatedAt);
+    this.#product = props.product;
+    this.#amount = props.amount;
+  }
+
+  readonly #product: Product;
+  readonly #amount: OrderItemAmount;
+}
+
+export namespace OrderItem {
+  export type CreateProps = BaseEntity.CreateProps & {
+    product: Product.CreateProps;
+    amount: number;
+  };
+
+  export type Props = BaseEntity.Props & {
+    product: Product;
+    amount: OrderItemAmount;
+  };
+
+  export type JSON = BaseEntity.JSON & {
+    product: Product.JSON;
+    amount: OrderItemAmount.JSON;
+  };
+}
---
+++ backend/src/modules/orders/domain/entities/order.entity.ts
@@ -0,0 +1,64 @@
+import { BaseEntity } from 'src/modules/common/domain';
+import z from 'zod';
+import { OrderItem } from './order-item.entity';
+
+export class Order extends BaseEntity {
+  public static get validator() {
+    return BaseEntity.baseValidator.extend({
+      items: z
+        .array(OrderItem.validator)
+        .transform((items) =>
+          items.map((itemProps) => new OrderItem(itemProps)),
+        ),
+      orderDate: z.date(),
+    });
+  }
+
+  public static create(props: Order.CreateProps): Order {
+    return new Order(Order.validator.parse(props));
+  }
+
+  public toJSON(): Order.JSON {
+    return {
+      id: this.id.toJSON(),
+      items: this.items.map((item) => item.toJSON()),
+      total: this.total,
+      orderDate: this.createdAt,
+      createdAt: this.createdAt.toISOString(),
+      updatedAt: this.updatedAt.toISOString(),
+    };
+  }
+
+  public get items(): ReadonlyArray<OrderItem> {
+    return [...this.#items];
+  }
+
+  public get total(): number {
+    return this.items.reduce((total, item) => total + item.price, 0);
+  }
+
+  constructor(props: Order.Props) {
+    super(props.id, props.createdAt, props.updatedAt);
+    this.#items = props.items;
+  }
+
+  readonly #items: OrderItem[];
+}
+
+export namespace Order {
+  export type CreateProps = BaseEntity.CreateProps & {
+    items: OrderItem.CreateProps[];
+    orderDate: Date;
+  };
+
+  export type Props = BaseEntity.Props & {
+    items: OrderItem[];
+    orderDate: Date;
+  };
+
+  export type JSON = BaseEntity.JSON & {
+    items: OrderItem.JSON[];
+    total: number;
+    orderDate: Date;
+  };
+}
