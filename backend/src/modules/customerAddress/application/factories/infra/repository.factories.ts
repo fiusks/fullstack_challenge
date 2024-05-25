fb7738ff9cba0fe578fa793be4255eb5d334d4e6
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 15:40:39 -0300

    refactor(back/customerAddress/application): remove CustomerRepository factory from customerAddress module

--- backend/src/modules/customerAddress/application/factories/infra/repository.factories.ts
+++ backend/src/modules/customerAddress/application/factories/infra/repository.factories.ts
@@ -1,11 +1,6 @@
 import { FastifyInstance } from 'fastify';
-import { PrismaCustomerRepository } from '../../../../customer';
 import { PrismaCustomerAddressRepository } from '../../../infra';
 
 export const customerAddressRepositoryFactory = (fastify: FastifyInstance) => {
   return new PrismaCustomerAddressRepository(fastify.prisma);
-};
-
-export const customerRepositoryFactory = (fastify: FastifyInstance) => {
-  return new PrismaCustomerRepository(fastify.prisma);
 };
