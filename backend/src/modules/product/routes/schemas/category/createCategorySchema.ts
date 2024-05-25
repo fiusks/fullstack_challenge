931e7967d8056a155c34e8d341497c9bf333f67e
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 18:34:49 -0300

    refactor(back/product/routes): remove empty sapaces from createCategorySchema

--- backend/src/modules/product/routes/schemas/category/createCategorySchema.ts
+++ backend/src/modules/product/routes/schemas/category/createCategorySchema.ts
@@ -2,8 +2,8 @@ export const createCategorySchema = {
   type: 'object',
   required: ['name', 'description', 'enabled'],
   properties: {
-    name: { type: ' string' },
-    description: { type: ' string' },
+    name: { type: 'string' },
+    description: { type: 'string' },
     enabled: { type: 'boolean' },
   },
 };
