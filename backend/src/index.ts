29f58342543dc438b1c933eb672a2299931eda78
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 11:15:25 -0300

    feat(back): expose customerRoutes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify';
+import customerRoutes from './modules/customerAddress/routes/customer.-address.routes';
 import prismaPlugin from './modules/common/infra/http/fastify';
 
 const app = fastify();
 
 app.register(prismaPlugin);
+
+customerRoutes(app);
 
 app.listen({ port: 3333 }, (err, address) => {
   if (err) {
