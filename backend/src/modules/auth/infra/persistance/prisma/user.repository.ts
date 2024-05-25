00c8a5cb6f41f858d0bf77a52986ce708a4af546
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 16:40:57 -0300

    feat(back/auth/infra): add UserRepository

---
+++ backend/src/modules/auth/infra/persistance/prisma/user.repository.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from '@prisma/client';
+import { User, UserRepository } from '~/modules/auth/domain';
+import { PrismaCustomer } from '~/modules/customer';
+
+export class PrismaUserRepository implements UserRepository {
+  constructor(private readonly prismaService: PrismaClient) {}
+
+  private convert(prismaCustomer: PrismaCustomer): User {
+    return User.create({
+      ...prismaCustomer,
+      hashedPassword: prismaCustomer.password,
+    });
+  }
+  public async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prismaService.customer.findFirst({
+      where: { email },
+      include: { address: true },
+    });
+
+    if (!user) return null;
+
+    return this.convert(user);
+  }
+
+  public async hasByCpf(cpf: string): Promise<boolean> {
+    const cpfCount = await this.prismaService.customer.count({
+      where: { cpf },
+    });
+
+    return cpfCount > 0;
+  }
+
+  public async hasByEmail(email: string): Promise<boolean> {
+    const emailCount = await this.prismaService.customer.count({
+      where: { email },
+    });
+
+    return emailCount > 0;
+  }
+  public async hasByUsername(username: string): Promise<boolean> {
+    const usernameCount = await this.prismaService.customer.count({
+      where: { username },
+    });
+
+    return usernameCount > 0;
+  }
+
+  public async save(user: User): Promise<void> {
+    await this.prismaService.customer.create({
+      data: {
+        cpf: user.cpf.toJSON(),
+        email: user.email.toJSON(),
+        id: user.id.toJSON(),
+        password: user.hashedPassword,
+        username: user.username.toJSON(),
+        name: user.name,
+      },
+    });
+  }
+}
