1b3b4ef501185883a5f588e801b477c4cb864839
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 20:31:12 -0300

    refactor(back): update import of customerAddressRoutes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,30 @@
+import env from './env';
+
 import fastify from 'fastify';
-import customerAddressRoutes from './modules/customerAddress/routes/customer.-address.routes';
 import prismaPlugin from './modules/common/infra/http/fastify/plugins/prisma';
 import productRoutes from './modules/product/routes/product.routes';
 import categoryRoutes from './modules/product/routes/category.routes';
 import customerRoutes from './modules/customer/routes/customer.routes';
+import fastifyJwt from '@fastify/jwt';
+import { loginRoute, signUpRoute } from './modules/auth/routes';
+import customerAddressRoutes from './modules/customerAddress/routes/customer-address.routes';
 
 const app = fastify();
 
 app.register(prismaPlugin);
+app.register(fastifyJwt, {
+  secret: 'my-super-secret',
+});
+
+signUpRoute(app);
+loginRoute(app);
 
 customerAddressRoutes(app);
 productRoutes(app);
 categoryRoutes(app);
 customerRoutes(app);
 
-app.listen({ port: 3333 }, (err, address) => {
+app.listen({ port: env.PORT }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
