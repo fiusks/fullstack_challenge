25c243a045f72d70699fe4ea15b9caea08128a07
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 12:06:02 -0300

    feat(back/product/application): add productDtos

---
+++ backend/src/modules/product/application/dtos/product/create-product.dto.ts
@@ -0,0 +1,21 @@
+import { Product } from 'src/modules/product/domain';
+
+export class ProductInputDto {
+  public readonly description: string;
+  public readonly enabled: boolean;
+  public readonly price: number;
+  public readonly stockQuantity: number;
+  public readonly image: string;
+  public readonly categoryId: string;
+  public readonly name: string;
+
+  constructor(product: Product, categoryId: string) {
+    this.name = product.name;
+    this.description = product.description;
+    this.enabled = product.enabled;
+    this.price = product.price;
+    this.stockQuantity = product.stockQuantity;
+    this.image = product.image;
+    this.categoryId = categoryId;
+  }
+}
---
+++ backend/src/modules/product/application/dtos/product/update-product.dto.ts
@@ -0,0 +1,21 @@
+import { Product } from 'src/modules/product/domain';
+
+export class UpdateProductInputDto {
+  public readonly id: string;
+  public readonly description: string;
+  public readonly price: number;
+  public readonly stockQuantity: number;
+  public readonly image: string;
+  public readonly categoryId: string;
+  public readonly name: string;
+
+  constructor(product: Product, categoryId: string) {
+    this.id = product.id.id;
+    this.categoryId = categoryId;
+    this.name = product.name;
+    this.description = product.description;
+    this.price = product.price;
+    this.stockQuantity = product.stockQuantity;
+    this.image = product.image;
+  }
+}
