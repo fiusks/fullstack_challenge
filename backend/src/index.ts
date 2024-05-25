37704484d12347d745a2464e277f60b926ad1cfc
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 16:12:46 -0300

    chore(back): add customer routes to app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,18 @@
 import fastify from 'fastify';
-import customerRoutes from './modules/customerAddress/routes/customer.-address.routes';
+import customerAddressRoutes from './modules/customerAddress/routes/customer.-address.routes';
 import prismaPlugin from './modules/common/infra/http/fastify/plugins/prisma';
 import productRoutes from './modules/product/routes/product.routes';
 import categoryRoutes from './modules/product/routes/category.routes';
+import customerRoutes from './modules/customer/routes/customer.routes';
 
 const app = fastify();
 
 app.register(prismaPlugin);
 
-customerRoutes(app);
+customerAddressRoutes(app);
 productRoutes(app);
 categoryRoutes(app);
+customerRoutes(app);
 
 app.listen({ port: 3333 }, (err, address) => {
   if (err) {
