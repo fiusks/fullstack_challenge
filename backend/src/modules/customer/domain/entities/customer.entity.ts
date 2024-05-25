7ea409f15d6b5d6a91a66842539b457bcf45f2d5
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 09:00:24 -0300

    refactor(back/customer/domain): remove empty line

--- backend/src/modules/customer/domain/entities/customer.entity.ts
+++ backend/src/modules/customer/domain/entities/customer.entity.ts
@@ -5,7 +5,6 @@ import { EntityId } from '../../../common/domain/value-objects/entity-id.vo';
 import { BaseEntity } from '../../../common/domain/entities/base-entity';
 import { CustomerAddress } from '../../../customerAddress/domain';
 
-
 export class Customer extends BaseEntity {
   public static get validator() {
     return BaseEntity.validator.extend({
