ec3e42f53d29a4be9e07957a7de0faf7d3ebbed1
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 22:26:15 -0300

    feat(front/components): add search component

---
+++ frontend/src/components/search.tsx
@@ -0,0 +1,12 @@
+export function Search() {
+    return (
+      <div className="flex flex-col items-center mt-6">
+        <input
+          type="text"
+          className="border border-gray-200 rounded-md p-3 text-xs w-96"
+          placeholder="O que você procura?"
+        />
+      </div>
+    );
+  }
+  
