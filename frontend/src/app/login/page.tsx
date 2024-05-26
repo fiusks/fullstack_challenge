649d490c8ec7d5431471c23e1fe6870a4bb30ea6
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 21:45:42 -0300

    feat(front/app/login): add company logo to login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-
+import Logo from "@/assets/grupo-boticario.svg";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-purple-500 to-pink-500">
       <div className="flex flex-col justify-center items-center p-8 bg-white rounded-xl drop-shadow-xl">
-        
+        <Image src={Logo} alt="Logo Boticário" style={{ height: 50, width: "auto" }} />
 
         <div className="flex flex-col items-center pb-5 pt-5">
           <h1 className="font-bold text-xl text-gray-500">Login</h1>
