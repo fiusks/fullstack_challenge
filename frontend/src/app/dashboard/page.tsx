c6acfd810a670efb59584f82ee9a18625c6540ab
Rafael <rafael.barros@kinvo.com.br>
2024-05-25 22:26:25 -0300

    feat(front/dashboard): add dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Header, ProductsList, Search } from "@/components";
+
+export default function Dashboard() {
+  return (
+    <main className="flex-1 flex-col items-center justify-between bg-neutral-50 rounded-lg">
+      <Header />
+      <Search />
+      <ProductsList />
+    </main>
+  );
+}
