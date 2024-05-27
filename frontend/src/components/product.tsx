56e6b2aec1775371708f7dc12aa3d344d917af24
Rafael <rafael.barros@kinvo.com.br>
2024-05-26 23:12:14 -0300

    feat(front/components): add cart hanlder on component

--- frontend/src/components/product.tsx
+++ frontend/src/components/product.tsx
@@ -1,203 +1,13 @@
 'use client'
 
+import { addToCart } from "@/lib/features/cartSlice";
 import { fetchProducts } from "@/lib/features/productSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { Product } from "@/modules/products/domain";
 import { convertCurrencyToLocaleBRL } from "@/utils/convertCurrencyToLocaleBRL";
 import Image from "next/image";
 import { useEffect } from "react";
 
-export const data = [
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef1234567890",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef123456723490",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-423ef1234567890",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef1254534567892",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef123456783",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef12345678324233",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-abcd-ef12345674234383",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6f-7890-a123bcd-ef123456783",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6123f-7890-abcd-ef123456783",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  },
-  {
-    id: "1a2b3c4d-5e6123f-7890-abcd-ef123456783",
-    name: "Sample Product",
-    description: "This is a sample product description",
-    price: 29.99,
-    enabled: true,
-    stockQuantity: 100,
-    image: "https://example.com/sample-product-image.jpg",
-    category: {
-      name: "lorem",
-      description: "lorem impsum",
-      createdAt: "2024-05-25T12:00:00.000Z",
-      updatedAt: "2024-05-25T12:30:00.000Z",
-      enabled: true,
-      id: "abcd1234-5678-90ef-ghij-klmnopqrstuvad"
-    },
-    createdAt: "2024-05-25T12:00:00.000Z",
-    updatedAt: "2024-05-25T12:30:00.000Z"
-  }
-];
 
 export function ProductsList() {
   const dispatch = useAppDispatch();
@@ -205,6 +15,10 @@ export function ProductsList() {
   useEffect(()=>{
     dispatch(fetchProducts())
   },[dispatch])
+
+  const handleAddToCart = (product: Product) => {
+    dispatch(addToCart(product));
+  };
 
   return (
     <section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center p-8">
@@ -228,6 +42,7 @@ export function ProductsList() {
             </p>
 
             <button
+              onClick={() => handleAddToCart(product)}
               type="submit"
               className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600 transition duration-200 ease-in-out text-xs"
             >
